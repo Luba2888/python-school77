9310f8174c4f4c3bc449e8a631279c354b7ba7bd
kuzminprog <kuzminprog@gmail.com>
2022-10-16 00:56:00 +0400

    Add examples to topic 3.2

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 
 -  [Тема 1. Основы языка Python](https://github.com/kuzminprog/python_school77/tree/main/module01/topic01)
 -  [Тема 2. Ввод и вывод данных](https://github.com/kuzminprog/python_school77/tree/main/module01/topic02)
--  [Тема 3. Работа с целыми числами](https://github.com/kuzminprog/python_school77/tree/main/module01/topic03)
+-  [Тема 3 Работа с целыми числами (Часть 1)](https://github.com/kuzminprog/python_school77/tree/main/module01/topic03_01)
+-  [Тема 3 Работа с целыми числами (Часть 2)](https://github.com/kuzminprog/python_school77/tree/main/module01/topic03_02)
---
+++ module01/topic03_02/example01.py
@@ -0,0 +1,70 @@
+# Арифметические операции с присвоением
+# Сложение
+sum = 20
+sum += 5
+print(sum)
+
+# Вычитание
+diff = 20
+diff -= 5
+print(diff)
+
+# Умножение
+prod = 20
+prod *= 5
+print(prod)
+
+# Деление
+div = 20
+div /= 5
+print(div)
+
+# Целочисленное деление
+div_int = 20
+div_int //= 6
+print(div_int)
+
+# Остаток от деления
+mod = 20
+mod %= 6
+print(mod)
+
+# Возведение в степень
+exp = 20
+exp **= 2
+print(exp)
+
+
+
+# Функции перевода чисел в различные системы счисления
+number = 220
+
+# Преобразование целого числа в двоичную строку
+binary = bin(number)
+print(binary)
+
+# Преобразование целого числа в шестнадцатеричную строку
+hexadecimal = hex(number)
+print(hexadecimal)
+
+# Преобразование целого числа в восьмеричную строку
+octal = oct(number)
+print(octal)
+
+
+
+# Запись числа в различных системах счисления
+# В двоичной системе
+from_bin = 0b1011101
+print(from_bin)
+
+# В шестнадцатеричной системе
+from_hex = 0xF48D
+print(from_hex)
+
+# В восьмеричной системе 
+from_oct = 0o7756
+print(from_oct)
+
+
+
