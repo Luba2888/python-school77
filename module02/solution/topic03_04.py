fc6c4b951a61d6d74f9d018e9ceb72b1ef505c0a
kuzminprog <kuzminprog@gmail.com>
2023-03-10 16:20:27 +0400

    fix solution

--- module02/solution/topic03_04.py
+++ module02/solution/topic03_04.py
@@ -21,18 +21,3 @@ while True:
 numbers.sort()
 for num in numbers:
     print(num)
-
-
-# Task 3
-numbers = [randint(-20, 20) for i in range(10)]
-print(numbers)
-print(len(numbers))
-print(numbers[-1])
-print(numbers[::-1])
-
-if 5 in numbers and 17 in numbers:
-    print('YES')
-else:
-    print('NO')
-
-print(numbers[1:-1])
